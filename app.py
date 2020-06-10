524883dd6cb1f49f1fa317556070bc46f2e4e449
Elnathan Au <elnathanau1@gmail.com>
2020-06-10 02:29:43 -0500

    bypasses cloudflare, scrapes, decodes and returns url(#4)

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import flask
 from os import environ
-# from flask import request, jsonify
-# from bs4 import BeautifulSoup
+from flask import request, jsonify
+from bs4 import BeautifulSoup
+import cfscrape
+import re
+import utility
 
 app = flask.Flask(__name__)
 # app.config["DEBUG"] = True
@@ -15,6 +18,31 @@ def home():
 def health():
     return "OK"
 
+@app.route('/get/episode', methods=['POST'])
+def create_task():
+    # validate request body
+    if not request.json or not 'url' in request.json:
+        flask.abort(400)
+
+    # get site contents
+    url = request.json['url']
+    scraper = cfscrape.create_scraper()
+    content = scraper.get(url).content
+
+    # beautifulsoup scrape
+    soup = BeautifulSoup(content, 'html.parser')
+    source = soup.find("iframe")["src"]
+    js = scraper.get("https://www1.animeultima.to" + source).content
+
+    js_soup = BeautifulSoup(js, 'html.parser')
+    script_tag = js_soup.findAll("script")
+    script = script_tag[-1].string
+    script = script.strip()
+
+    unpacked = eval('utility.unpack' + script[script.find('}(') + 1:-1])
+    download_link = re.match(r'''var fone="(.*?)";''', unpacked)[1]
+
+    return jsonify("download_link", download_link), 200
 
 if __name__ == '__main__':
     app.run(environ.get('PORT'))
