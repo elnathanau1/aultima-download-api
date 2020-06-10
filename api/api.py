c4a19430e6dd37b033aa8306971999e0b9655df6
elnathanau1 <elnathanau1@gmail.com>
2020-06-09 23:37:59 -0500

    init

---
+++ api/api.py
@@ -0,0 +1,17 @@
+import flask
+from flask import request, jsonify
+from bs4 import BeautifulSoup
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>aultima-api-flask</h1><p>This is an api for retrieving download links from aultima.</p>"
+
+@app.route('/health', methods=['GET'])
+def health():
+    return "OK"
+
+app.run()
